42a2cee1e22a1df723c59210c8ab163b9cd67571
Exentirion <oli-fuchs@live.de>
2023-11-08 14:16:49 +0100

    Completely Implemented UserJsonUtil (except close??)

--- Backend/hausverwaltung/src/main/java/dev/hv/rest/util/UserJsonUtil.java
+++ Backend/hausverwaltung/src/main/java/dev/hv/rest/util/UserJsonUtil.java
@@ -15,58 +15,71 @@ import dev.hv.db.model.IDUser;
 import dev.hv.rest.model.IRUser;
 import dev.hv.rest.model.RUser;
 
-public class UserJsonUtil implements IUsers{
+public class UserJsonUtil implements IUsers {
 
 	private Jdbi connection = IDb.getInstance().getJdbi();
-	
+
 	public UserJsonUtil() {
 		connection.installPlugin(new SqlObjectPlugin());
 		connection.installPlugin(new GuavaPlugin());
 	}
-	
+
 	@Override
 	public void close() {
-		
+
 	}
 
 	@Override
 	public void delete(Integer id) {
-		
+		Handle handle = connection.open();
+
+		final IUserDAO dao = handle.attach(IUserDAO.class);
+
+		dao.delete(id);
 	}
 
 	@Override
 	public List<IRUser> getAll() {
 		Handle handle = connection.open();
-		
+
 		final IUserDAO dao = handle.attach(IUserDAO.class);
 		List<DUser> DatabaseUsers = dao.getAll();
-		
-		
+
 		List<IRUser> RestUsers = new ArrayList();
 		for (DUser user : DatabaseUsers) {
 			RestUsers.add(new RUser(user));
 		}
-		
+
 		return RestUsers;
 	}
 
 	@Override
 	public IRUser getWithID(Integer id) {
 		Handle handle = connection.open();
-		
+
 		final IUserDAO dao = handle.attach(IUserDAO.class);
-		
+
 		return new RUser(dao.findById(id));
 	}
 
 	@Override
 	public int insert(IRUser user) {
-		return 0;
+		Handle handle = connection.open();
+
+		final IUserDAO dao = handle.attach(IUserDAO.class);
+		DUser dbUser = new DUser(user);
+
+		return dao.insert(dbUser);
 	}
 
 	@Override
 	public void update(IRUser user) {
-		
+		Handle handle = connection.open();
+
+		final IUserDAO dao = handle.attach(IUserDAO.class);
+		DUser dbUser = new DUser(user);
+
+		dao.update(dbUser);
 	}
 
 }
