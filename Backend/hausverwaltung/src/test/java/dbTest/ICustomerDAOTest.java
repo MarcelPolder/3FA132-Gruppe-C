6efc5566dc55dc76252586879f4f16ed3b88b616
Marcel Polder <m.polder@nakami.de>
2023-11-08 10:58:28 +0100

    fix test bugs

--- Backend/hausverwaltung/src/test/java/dbTest/ICustomerDAOTest.java
+++ Backend/hausverwaltung/src/test/java/dbTest/ICustomerDAOTest.java
@@ -1,3 +1,5 @@
+package dbTest;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
--- Backend/hausverwaltung/src/test/java/dbTest/IUserDAOTest.java
+++ Backend/hausverwaltung/src/test/java/dbTest/IUserDAOTest.java
@@ -1,3 +1,4 @@
+package dbTest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
