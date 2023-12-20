e79e7b1a173bf9fcbce601905c9bd117818b535b
Moritz Krug <moritz.krug@veridos.com>
2023-12-20 10:42:48 +0100

    adding Unit Test for RestServer

---
+++ Backend/hausverwaltung/src/test/java/dbTest/RestServerTest.java
@@ -0,0 +1,15 @@
+package dbTest;
+
+import dev.hv.rest.Server;
+import org.junit.Test;
+
+
+
+public class RestServerTest {
+
+    @Test
+    public void testMainMethod() {
+        Server.main(null);
+
+    }
+}
