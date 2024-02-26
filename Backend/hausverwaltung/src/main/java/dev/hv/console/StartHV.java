7ee0b533d1f0623e47a2a81fc3ff3dbabebc9a88
Marcel Polder <m.polder@nakami.de>
2024-02-26 11:36:11 +0100

    StartHV impl

--- Backend/hausverwaltung/src/main/java/dev/hv/console/StartHV.java
+++ Backend/hausverwaltung/src/main/java/dev/hv/console/StartHV.java
@@ -1,19 +1,25 @@
 package dev.hv.console;
 
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
 public class StartHV {
     public static void main(String[] args) {
         System.out.println("Hello, this is the HV console client!");
-
-        if (args.length > 0 && args[0].equals("-h")) {
-            printHelp();
-        } else {
         System.out.println("Arguments:");
         for (String arg : args) {
             System.out.println(arg);
         	}
+        
+        if (args.length > 0 && args[0].equals("-h")) {
+            printHelp();
+        } else if (args.length > 0 && args[0].equals("--delete")) {
+        	//delete all
+        } else { 
+        	System.out.println("Error resolving Arguments!");
         }
     }
     public static void printHelp() {
+    	System.out.printf("!javaversion%s%n", System.getProperty("java.version"));
     	System.out.println("Help");
     	System.out.println("Here is a list of all Arguments:");
     	//list all args
