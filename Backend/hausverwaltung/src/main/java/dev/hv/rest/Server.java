8622998676087c860921618d71dc6100c621dd24
Moritmer Gibbons <mkirchermeier@gmail.com>
2024-03-01 09:33:02 +0100

    Fix: Errors

--- Backend/hausverwaltung/src/main/java/dev/hv/rest/Server.java
+++ Backend/hausverwaltung/src/main/java/dev/hv/rest/Server.java
@@ -1,49 +1,20 @@
 package dev.hv.rest;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.URI;
-import java.util.Properties;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.sun.net.httpserver.HttpServer;
-import sun.misc.Signal;
 
 public class Server {
-
-	ResourceConfig resources;
-	String url;
-	Properties appProps;
-	HttpServer httpServer;
-
-	public Server() {
-		appProps = new Properties();
-		try {
-			appProps.load(new FileInputStream(getClass().getResource("/app.properties").getPath()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		resources = new ResourceConfig().packages(appProps.getProperty("API_RESOURCES"));
-		url = "http://"+appProps.getProperty("API_HOST")+":"+appProps.getProperty("API_PORT")+"/";
-	}
-
-	public boolean open() {
-		httpServer = JdkHttpServerFactory.createHttpServer(URI.create(this.url), this.resources);
-		System.out.println("The server is now listening on "+this.url);
-		return true;
-	}
-
-	public boolean close() {
-		this.httpServer.stop(0);
-		System.out.println("The server is now closed.");
-		return true;
-	}
 	
 	public static void main (String[] args) {
-		Server server = new Server();
-		server.open();
-		Signal.handle(new Signal("INT"), signal -> server.close());
+		final String pack = "dev.hv.rest.resources";
+		String url = "http://localhost:8080/rest";
+		final ResourceConfig rc = new ResourceConfig().packages(pack);
+		final HttpServer server = JdkHttpServerFactory.createHttpServer(
+		URI.create(url), rc);
+		System.out.println("The server is now listening on "+server.getAddress().getHostName()+":"+server.getAddress().getPort());
 	}
 }
