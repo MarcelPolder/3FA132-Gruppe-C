36408823b5b5ca23d59808e083e4d17352ba9b94
Exentirion <oli-fuchs@live.de>
2024-03-04 11:51:44 +0100

    Added ON DELETE SET NULL Constraint to Readings/Customer Relation and Activated Foreign Keys with PRAGMA foreign_keys = ON;

--- Backend/hausverwaltung/src/main/resources/db/schema.sql
+++ Backend/hausverwaltung/src/main/resources/db/schema.sql
@@ -1,23 +1,13 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE reading (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	comment TEXT,
-	customer_id INTEGER,
-	date_of_reading TEXT,
-	kind_of_meter TEXT,
-	meter_count INTEGER,
-	meter_id TEXT,
-	substitute INTEGER DEFAULT (0),
-	CONSTRAINT reading_customer_fk FOREIGN KEY (customer_id) REFERENCES "customer"(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT, comment TEXT, customer_id INTEGER, date_of_reading TEXT, kind_of_meter TEXT, meter_count INTEGER, meter_id TEXT, substitute INTEGER DEFAULT(0), CONSTRAINT reading_customer_fk FOREIGN KEY (customer_id) REFERENCES "customer" (id) ON DELETE SET NULL
 );
+
 CREATE TABLE customer (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	vorname TEXT,
-	nachname TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT, vorname TEXT, nachname TEXT
 );
+
 CREATE TABLE user (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	firstname TEXT,
-	lastname TEXT,
-	password TEXT,
-	token TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, password TEXT, token TEXT
 );
